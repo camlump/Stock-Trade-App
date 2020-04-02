53873dc286f098effc0e8df65b65346720bef68c
CameronLumpkinATL <camlump@yahoo.com>
2020-04-01 20:17:35 -0400

    initial setup

---
+++ client/build/precache-manifest.5ec439692e5c7d1bad8d2eb663e193ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4610b21ac6e44b5d365ea99f7f90d671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27e132f1103ce4cb929",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "a27e132f1103ce4cb929",
+    "url": "/static/js/main.cbde5ba1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
